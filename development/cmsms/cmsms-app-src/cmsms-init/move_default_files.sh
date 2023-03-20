855bcdf2cf1b83a667c30bd05c23f4f48b929c9f
OLED1 <oliver@edtmair.at>
2023-03-20 08:13:07 +0100

    Added symlink to  uploads directory for module custom.

--- development/cmsms/cmsms-app-src/cmsms-init/move_default_files.sh
+++ development/cmsms/cmsms-app-src/cmsms-init/move_default_files.sh
@@ -2,6 +2,9 @@
 echo "[STEP]Copying default files."
 find /var/www/html/uploads -maxdepth 0 -type d -empty -exec cp -R /var/www/html/uploads.default/* /var/www/html/uploads \;
 find /var/www/html/modules -maxdepth 0 -type d -empty -exec cp -R /var/www/html/modules.default/* /var/www/html/modules \;
+echo "[STEP]Creating and symlinking module_custom folder."
+mkdir -p /var/www/html/uploads/assets/module_custom
+ln -s /var/www/html/uploads/assets/module_custom/* /var/www/html/assets/module_custom/
 echo "[STEP]Setting default rights www-data:www-data for uploads and modules directory."
 chown -R www-data:www-data /var/www/html/uploads
 chown -R www-data:www-data /var/www/html/modules
