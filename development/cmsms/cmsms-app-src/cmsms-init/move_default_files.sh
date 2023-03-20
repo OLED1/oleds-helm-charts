6187f52a92f8b1350c842381af68b3e0e7fbdefe
OLED1 <oliver@edtmair.at>
2023-03-20 15:43:07 +0100

    New CMSMS Version.

--- development/cmsms/cmsms-app-src/cmsms-init/move_default_files.sh
+++ development/cmsms/cmsms-app-src/cmsms-init/move_default_files.sh
@@ -4,7 +4,9 @@ find /var/www/html/uploads -maxdepth 0 -type d -empty -exec cp -R /var/www/html/
 find /var/www/html/modules -maxdepth 0 -type d -empty -exec cp -R /var/www/html/modules.default/* /var/www/html/modules \;
 echo "[STEP]Creating and symlinking module_custom folder."
 mkdir -p /var/www/html/uploads/assets/module_custom
-ln -s /var/www/html/uploads/assets/module_custom/* /var/www/html/assets/module_custom/
+cp -f /var/www/html/assets/module_custom/index.html /var/www/html/uploads/assets/module_custom/
+rm -rf /var/www/html/assets/module_custom
+ln -s /var/www/html/uploads/assets/module_custom /var/www/html/assets/module_custom
 echo "[STEP]Setting default rights www-data:www-data for uploads and modules directory."
 chown -R www-data:www-data /var/www/html/uploads
 chown -R www-data:www-data /var/www/html/modules
