03ee449d981947c6b1680e177224eebdd0940ffd
OLED1 <oliver@edtmair.at>
2023-03-20 08:07:35 +0100

    Bug fix copy files in init script. Added maxdepth.

--- development/cmsms/cmsms-app-src/cmsms-init/move_default_files.sh
+++ development/cmsms/cmsms-app-src/cmsms-init/move_default_files.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 echo "[STEP]Copying default files."
-find /var/www/html/uploads -type d -empty -exec cp -R /var/www/html/uploads.default/* /var/www/html/uploads \;
-find /var/www/html/modules -type d -empty -exec cp -R /var/www/html/modules.default/* /var/www/html/modules \;
+find /var/www/html/uploads -maxdepth 0 -type d -empty -exec cp -R /var/www/html/uploads.default/* /var/www/html/uploads \;
+find /var/www/html/modules -maxdepth 0 -type d -empty -exec cp -R /var/www/html/modules.default/* /var/www/html/modules \;
+echo "[STEP]Setting default rights www-data:www-data for uploads and modules directory."
 chown -R www-data:www-data /var/www/html/uploads
 chown -R www-data:www-data /var/www/html/modules
